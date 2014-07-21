d16c0f4735f9130017c919c592e2273e84d4acd5
Junichi Kobayashi <nnujabok@gmail.com>
2014-07-21 19:27:23 +0900

    first commit

---
+++ src/jp/enixer/gdskillmanager/GDSkillManager.java
@@ -0,0 +1,15 @@
+package jp.enixer.gdskillmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class GDSkillManager extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gdskill_manager);
+	}
+}
